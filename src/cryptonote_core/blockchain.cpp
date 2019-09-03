f6f1264ff29c6407368067f5033b206fd1cf5a39
zooteld <zooteld@gmail.com>
2019-09-02 19:59:03 -0500

    return to main chain

--- src/cryptonote_core/blockchain.cpp
+++ src/cryptonote_core/blockchain.cpp
@@ -85,7 +85,6 @@ static const struct
   { 2, 21300, 0, 1497657600 },
   { 3, 72000, 0, 1524577218 }, // Roughly the 20th of April.
   { 4, 208499, 0, 1531762611 } // Roughly the 23rd of July.
-
 };
 
 static const uint64_t mainnet_hard_fork_version_1_till = (uint64_t)-1;
